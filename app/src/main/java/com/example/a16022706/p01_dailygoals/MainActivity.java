756e8a1028be3b6253d25bb6025503b00210d5e6
16022706 <16022706@myrp.edu.sg>
2018-04-16 23:59:40 +0800

    goals

--- app/src/main/java/com/example/a16022706/p01_dailygoals/MainActivity.java
+++ app/src/main/java/com/example/a16022706/p01_dailygoals/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a16022706.p01_dailygoals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOk =  (Button)findViewById(R.id.okBtn);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                EditText etName =  (EditText) findViewById(R.id.editTextName);
+                EditText etAge =  (EditText) findViewById(R.id.editTextAge);
+                String[] info = {etName.getText().toString(),etAge.getText().toString()};
+
+                Intent i = new Intent(MainActivity.this,DemoActivities2.class);
+
+                i.putExtra("info",info);
+
+                startActivity(i);
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/a16022706/p01_dailygoals/SecondActivity.java
@@ -0,0 +1,8 @@
+package com.example.a16022706.p01_dailygoals;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    
+}
